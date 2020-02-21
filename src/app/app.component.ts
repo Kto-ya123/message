7d4d2bcfe1b6014aab3d54c638e0788ba559cbfa
Artur <artur.luzgin.luzgin@mail.ru>
2020-02-21 16:36:36 +0300

    add main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { User } from '../assets/user';
+import { NgForm } from '@angular/forms';
+import {MenuItem, MessageService} from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService]
 })
-export class AppComponent {
-  title = 'message';
+export class AppComponent /*implements OnInit*/ {
+  title = 'AppComponent';
+  users: Array<User> = [];
+  name: string;
+  age: number;
+  items: MenuItem[];
+  constructor(private messageService: MessageService) {}
+  ngOnInit() {
+    this.items = [
+      {label: 'Delete', icon: 'pi pi-refresh', command: () => {
+          this.getIndexUser(new User('Artur', 24));
+        }}
+    ];
+  }
+  addUser(myForm: NgForm): void {
+    const user: User = new User( this.name, this.age );
+    if (this.getIndexUser(user) === -1) {
+      this.users.push(user);
+    }
+  }
+  deleteUser(myForm: NgForm): void {
+    const user: User = new User( this.name, this.age );
+    const indexUser: number = this.getIndexUser(user);
+    if(indexUser >= 0) {
+      this.users.splice(indexUser, 1);
+    }
+  }
+  getIndexUser(user: User): number {
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users[i].equals(new User(this.name, this.age))) {
+        return i;
+      }
+    }
+    return -1;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SplitButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
